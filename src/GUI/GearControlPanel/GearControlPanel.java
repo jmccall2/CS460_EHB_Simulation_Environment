670b042c523177d4c65c125fdbd6611f993c4512
CS-Labs <cseely@unm.edu>
2018-02-11 21:01:25 -0700

    gears.

--- src/GUI/GearControlPanel/GearControlPanel.java
+++ src/GUI/GearControlPanel/GearControlPanel.java
@@ -2,18 +2,23 @@ package GUI.GearControlPanel;
 
 import GUI.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class GearControlPanel implements GUIPanel
 {
     private BorderPane _gearControlPanel;
+    private Shifter _shifter;
     public GearControlPanel()
     {
         _gearControlPanel = new BorderPane();
-        BackgroundFill myBF = new BackgroundFill(Color.BROWN, new CornerRadii(1),
+        BackgroundFill myBF = new BackgroundFill(Color.BLACK, new CornerRadii(1),
                 new Insets(0.0,0.0,0.0,0.0));
         _gearControlPanel.setBackground(new Background(myBF));
+        _shifter = new Shifter();
+        _gearControlPanel.setCenter(_shifter.getHBox());
+      //  _gearControlPanel.setAlignment(_shifter.getHBox(), Pos.CENTER);
     }
 
     public BorderPane getPanel()
