5b8a32537f1d9389fda381d0294ef5b3bceaa204
CS-Labs <cseely@unm.edu>
2018-02-11 22:16:33 -0700

    Adding very pretty button.

--- src/GUI/SimulationInitPanel/InitialParametersPanel.java
+++ src/GUI/SimulationInitPanel/InitialParametersPanel.java
@@ -1,6 +1,7 @@
 package GUI.SimulationInitPanel;
 
 import GUI.*;
+import GUI.SimulationScreen.ExternalView.SimulationPanel;
 import javafx.geometry.Insets;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -14,6 +15,8 @@ public class InitialParametersPanel implements GUIPanel
         BackgroundFill myBF = new BackgroundFill(Color.ORANGE, new CornerRadii(1),
                 new Insets(0.0,0.0,0.0,0.0));
         _initParamsPanel.setBackground(new Background(myBF));
+        SimulationStateButton _simStateButton = new SimulationStateButton();
+        _initParamsPanel.setCenter(_simStateButton.getButton());
     }
 
     public BorderPane getPanel()
--- src/GUI/SimulationInitPanel/SimulationStateButton.java
+++ src/GUI/SimulationInitPanel/SimulationStateButton.java
@@ -1,4 +1,46 @@
 package GUI.SimulationInitPanel;
 
-public class SimulationStateButton {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public class SimulationStateButton
+{
+    private Button _simStateButton;
+    private boolean _simOn;
+    SimulationStateButton()
+    {
+        _simOn = false;
+        _simStateButton = new Button("Start Simulation");
+        _simStateButton.setId("simStateButton");
+        _simStateButton.getStylesheets().add(getClass().getResource("/resources/css/simStartButton.css").toExternalForm());
+        _simStateButton.applyCss();
+        EventHandler<ActionEvent> _buttonHandler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                _toggleButtonState();
+                // other stuff.
+            }
+        };
+        _simStateButton.setOnAction(_buttonHandler);
+    }
+
+    public Button getButton()
+    {
+        return _simStateButton;
+    }
+
+    private void _toggleButtonState()
+    {
+        System.out.println("Toggle simulator state.");
+        _simOn = !_simOn;
+        _simStateButton.getStylesheets().clear();
+        String css = _simOn ? "/resources/css/simStopButton.css" : "/resources/css/simStartButton.css";
+        String buttonText = _simOn ? "Stop Simulation" : "Start Simulation";
+        _simStateButton.setText(buttonText);
+        _simStateButton.getStylesheets().add(getClass().getResource(css).toExternalForm());
+    }
+
+
+
 }
