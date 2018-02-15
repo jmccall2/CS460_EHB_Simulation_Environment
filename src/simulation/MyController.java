d7077946af3090aa82e7ba3e209c0893c95a820c
Jeffrey McCall <jmccall2@unm.edu>
2018-02-15 11:01:11 -0700

    Merging.

--- src/simulation/MyController.java
+++ src/simulation/MyController.java
@@ -5,34 +5,62 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
 //Controller for FXML pane.
 public class MyController implements Initializable
 {
   @FXML private TextField setSpeedField;
   @FXML private TextField currSpeedField;
   @FXML private TextField pressureField;
-  @FXML private ToggleButton start_stop_sim;
-  @FXML private ToggleButton handBrake;
+  @FXML private Button start_stop_sim;
+  @FXML private Button handBrake;
   @FXML private RadioButton parkButton;
   @FXML private RadioButton neutralButton;
   @FXML private RadioButton driveButton;
   @FXML private RadioButton firstGear;
   @FXML private RadioButton secondGear;
+  private boolean stopped = true;
   
   @Override
   public void initialize(URL arg0, ResourceBundle arg1)
   {
- 
+    start_stop_sim.setOnAction((event) -> {
+      if(stopped)
+      {
+        stopped = false;
+        start_stop_sim.setText("Stop simulation.");
+      }
+      else if(!stopped)
+      {
+        stopped = true;
+        start_stop_sim.setText("Start simulation.");
+      } 
+    });
   }
+  
   
   public void setSpeed(double speed)
   {
     String speedStr = Double.toString(speed);
     currSpeedField.setText(speedStr);
   }
+  
+//  @FXML
+//  private void handleButtonAction(ActionEvent event)
+//  {
+//    if(stopped)
+//    {
+//      stopped = false;
+//      start_stop_sim.setText("Stop simulation.");
+//    }
+//    else if(!stopped)
+//    {
+//      stopped = true;
+//      start_stop_sim.setText("Start simulation.");
+//    }
+//  }
 //
 //  public void setAcceleration(double acceleration)
 //  {
