1ba10d1fc045a30522a1e81399a0284404f1ffea
Christian Seely <cseely@unm.edu>
2018-02-18 21:44:13 -0700

    Update ApplicationEntryPoint.java

--- src/simulation/ApplicationEntryPoint.java
+++ src/simulation/ApplicationEntryPoint.java
@@ -120,7 +120,7 @@ public class ApplicationEntryPoint implements PulseEntity{
 
     @Override
     public void pulse(double deltaSeconds) {
-        _ehb.update();
+        _ehb.init();
         if(init)_gui.setInitColor();
         init = false;
     }
