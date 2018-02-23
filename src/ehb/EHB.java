d37384528c25664df9674f7136c32bdbbdd2a7b9
Justin <amunra@unm.edu>
2018-02-22 17:14:47 -0700

    Minor comment change

--- src/ehb/EHB.java
+++ src/ehb/EHB.java
@@ -7,7 +7,7 @@ public class EHB
    * TODO: Implement me
    *
    * Method called by the engine with each simulation tick.
-   * Do not place any loops in this method.
+   * Do not place any infinite loops in this method.
    *
    * @param deltaSeconds Change in seconds since the last update.
    *                     If the simulation.engine is running at 60 frames per second,
